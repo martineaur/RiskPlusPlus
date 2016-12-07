13ab79674aa2b8046eee8723b5e51c0a64b4e2a4
rreid12 <reidr@wit.edu>
2016-12-06 21:59:59 -0500

    completely commented player.h

--- player.h
+++ player.h
@@ -10,6 +10,12 @@
 #include "country.h"
 using namespace std;
 
+/*
+ * The player class describes a player and their role in the game of RiskPlusPlus. For each
+ * player, it is important to keep track of their ID, their number of armies, and the
+ * countries they control. This class is supposed to handle all of the functionality of a
+ * player in the game of Risk
+ */
 
 class Player {
 
@@ -32,26 +38,37 @@ class Player {
 			//armies/troops
 
 			Player(){
+				//default constructor
 				static int p = 0;
 				p++;
 				playerId = p;
 				setArmiesAvailable();
 				cout << "A player has entered the game. His Player ID is " << playerId << endl;
 			}
+			/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+			 * Constructor simply assigns the player an ID number, gives him the game's set
+			 * number of armies (8), and tells the user by printing to the console.
+			 */
 
 			vector<Country*> countriesControlled;
+			/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+			 * This is a major component of the Player type. This vector contains all the countries
+			 * a player controls at any one time.
+			 */
 
 	private:
+			//simple attributes for Player type
 			int playerId;
 			int armiesAvailable;
-			//an integer between 0 and 8 that uses riskMap to find the country
-
 
 };
 
 int Player::getId() {
 	return playerId;
 }
+/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * Returns the value of the player's ID, set at instantiation
+ */
 
 void Player::setArmiesAvailable(){
 	armiesAvailable = 8;
@@ -64,6 +81,11 @@ void Player::setArmiesAvailable(int a){
 int Player::getArmiesAvailable() {
 	return armiesAvailable;
 }
+/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * Setters and getters for the armiesAvailable attribute. setArmiesAvailable is overloaded because
+ * it needs to be set to 8 automatically when the player is instantiated. After that, the function may
+ *  need to be called to set a different amount, so it is helpful to overload it.
+ */
 
 void Player::giveArmyToCountry(Country* ctry) {
 	armiesAvailable--;
@@ -74,6 +96,12 @@ void Player::giveArmyToCountry(Country* ctry) {
 		}
 	}
 }
+/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * This function takes one of the armies available to this player, and places it into a country that is
+ * owned by the player. The countriedControlled vector is searched by an iterator from beginning to end,
+ * looking for a matching country pointer. Once the matching country pointer is found, the
+ * addOccupiedArmies() function is called from the Country class on that Country
+ */
 
 void Player::becomeOwner(Country* ctry) {
 
@@ -84,6 +112,12 @@ void Player::becomeOwner(Country* ctry) {
 	ctry->setOwner(this->getId());
 
 }
+/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * The becomeOwner() function allows the player to take control of a country by adding a pointer to that
+ * country to its controlledCountries vector. This function used the built-in push_back function that is
+ * in the vector template class. This function also goes and sets the country's owner attribute to the ID
+ * of THIS player
+ */
 
 void Player::printControlledCountries() {
 
@@ -93,16 +127,20 @@ void Player::printControlledCountries() {
 	}
 
 }
+/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * This function loops through the countriesControlled vector and prints out the name of each country that
+ * this player owns, along with the number of armies in those countries
+ */
 
 //crying over a bowl of ramen while trying to chug down a redbull
 
 void Player::giveUpCountry(Country* ctry){
-	/*for(vector<Country*>::const_iterator it = countriesControlled.begin(); it != countriesControlled.end(); ++it){
-		if((*it)->getName() == ctry->getName()){
-			countriesControlled.remove((*it));
-		}
-	}*/
 
 	countriesControlled.erase(std::remove(countriesControlled.begin(), countriesControlled.end(), ctry), countriesControlled.end());
 
 }
+/* ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ * This function uses the erase function in combination with the remove function to allow the program to remove
+ * a country from the player's countriesControlled vector. It traverses through the vector, looking for a specifc
+ * country pointer, and once it is found, it is erased from the player's countriesControlled vector
+ */
